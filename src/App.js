646b037b69993a0354d671d8f8b4573035f03bd2
Tim Kholod <artijoh@gmail.com>
2018-01-11 02:02:54 +0300

    Simplify picking an application to render

--- src/App.js
+++ src/App.js
@@ -15,21 +15,22 @@ function GridItem({title, icon, isActive, onHover, onClick}) {
   )
 }
 
+const NA = <div>XXX</div>;
 
 class App extends Component {
   state = {
     items: [
-      {title: 'Jaguar', icon: 'https://listverse.com/wp-content/uploads/2012/11/jaguar-mouth.jpg'},
-      {title: 'Koala', icon: 'https://media.nationalgeographic.org/assets/photos/000/337/33744_r400x300.jpg?25dbc0b6d882f0283b7ce5960f7057c3b8513ab8'},
-      {title: 'Lion', icon: 'https://imgc.allpostersimages.com/img/print/karine-aigner-full-frame-close-up-portrait-of-a-male-white-lion-with-blue-eyes-south-africa_u-L-Q11019F0.jpg?src=gp'},
-      {title: 'Hedgehog', icon: 'http://hedgehoghannah.com/wp-content/uploads/2015/02/hedgehog3-800x600-400x300.jpg'},
-      {title: 'Wolforator', icon: 'https://static.howstuffworks.com/gif/grey-wolf-pups.jpg'},
-      {title: 'Puppy', icon: 'http://img.timesnownews.com/story/1511485704-puppy.JPG?d=400x300'},
-      {title: 'Squirrel', icon: 'https://i.pinimg.com/originals/81/d9/ed/81d9ed4192f789db752c0a9ee7430dd9.jpg'},
-      {title: 'Catymiders', icon: 'https://scottoline.com/wp-content/uploads/photo-gallery/animal3.jpg'},
-      {title: 'Rabbit', icon: 'https://www.hpcimedia.com/images/website/ManChemNews/DIR_55/F_37485.jpg'},
-      {title: 'Cow', icon: 'http://www.efeedlink.com/cps/images/2017/october/2017100615251759975105.gif'},
-      {title: 'Gazelle', icon: 'https://frankvanlangevelde.files.wordpress.com/2014/01/img_9723-e1487795906325.jpg?w=400'},
+      {title: 'Jaguar', component: <NA />, icon: 'https://listverse.com/wp-content/uploads/2012/11/jaguar-mouth.jpg'},
+      {title: 'Koala', component: <NA />, icon: 'https://media.nationalgeographic.org/assets/photos/000/337/33744_r400x300.jpg?25dbc0b6d882f0283b7ce5960f7057c3b8513ab8'},
+      {title: 'Lion', component: <NA />, icon: 'https://imgc.allpostersimages.com/img/print/karine-aigner-full-frame-close-up-portrait-of-a-male-white-lion-with-blue-eyes-south-africa_u-L-Q11019F0.jpg?src=gp'},
+      {title: 'Hedgehog', component: <NA />, icon: 'http://hedgehoghannah.com/wp-content/uploads/2015/02/hedgehog3-800x600-400x300.jpg'},
+      {title: 'Wolforator', component: <Calc />, icon: 'https://static.howstuffworks.com/gif/grey-wolf-pups.jpg'},
+      {title: 'Puppy', component: <NA />, icon: 'http://img.timesnownews.com/story/1511485704-puppy.JPG?d=400x300'},
+      {title: 'Squirrel', component: <NA />, icon: 'https://i.pinimg.com/originals/81/d9/ed/81d9ed4192f789db752c0a9ee7430dd9.jpg'},
+      {title: 'Catymiders', component: <div>CAT</div>, icon: 'https://scottoline.com/wp-content/uploads/photo-gallery/animal3.jpg'},
+      {title: 'Rabbit', component: <NA />, icon: 'https://www.hpcimedia.com/images/website/ManChemNews/DIR_55/F_37485.jpg'},
+      {title: 'Cow', component: <NA />, icon: 'http://www.efeedlink.com/cps/images/2017/october/2017100615251759975105.gif'},
+      {title: 'Gazelle', component: <NA />, icon: 'https://frankvanlangevelde.files.wordpress.com/2014/01/img_9723-e1487795906325.jpg?w=400'},
     ],
     activeItemIndex: 0,
     activeScreen: 'homescreen',
@@ -84,12 +85,8 @@ class App extends Component {
               />
             ))}
           </div>
-        ): this.state.activeScreen === 'Wolforator' ? (
-          <Calc />
-        ): this.state.activeScreen === 'Catymiders' ? (
-          <div>CAT</div>
-        ): (
-          <div>XXX</div>
+        ) : (
+          this.state.items.find(item => item.title === this.state.activeScreen).component
         )
       }
     </div>
